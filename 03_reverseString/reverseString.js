060ebbb5124cc97356ecf9aa3c32d448af20468f
Christopher Krieps <chriskrieps@gmail.com>
2022-08-09 14:40:29 -0500

    Complete javascript exercises 01-04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(originalString) {
+    let originalChar = Array.from(originalString);
+    len = originalChar.length;
 
+    let newChar = Array(len);
+    for (let i=0; i<(len+1); i++)
+    {
+        newChar[i] = originalChar[len-i];
+    }
+    newString = newChar.join("");
+    return newString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, removal /* */) {
+    // Get array of given arguments
+    const args = Array.from(arguments);
+
+    // Remove first argument as it is my array
+    args.shift();
+
+    // Filter my array by the remaining arguments
+    let filteredArray = myArray.filter(function(value, index, arr){
+        if (!args.includes(value)) return value;
+    })
+
+    // Return the now filtered array
+    return filteredArray;
 
 };
 
